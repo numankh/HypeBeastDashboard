ee0c5f11d02afe4fb6adb2765c773a1c35b7308e
numankh <khannuman15@gmail.com>
2021-12-12 09:22:39 -0500

    remove unused navbar links

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -11,7 +11,7 @@ export default function CustomNavbar() {
                 <Navbar.Brand href="#home">HypeBeastHelper</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
+                {/* <Nav className="me-auto">
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
                     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
@@ -23,11 +23,11 @@ export default function CustomNavbar() {
                     </NavDropdown>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
+                    <Nav.Link href="#deets">Test</Nav.Link>
                     <Nav.Link eventKey={2} href="#memes">
                     Dank memes
                     </Nav.Link>
-                </Nav>
+                </Nav> */}
                 </Navbar.Collapse>
                 </Container>
             </Navbar>
