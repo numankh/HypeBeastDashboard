ab5876e3af64ec6c5369350352dea4c19f69fccb
numankh <khannuman15@gmail.com>
2021-12-13 10:47:02 -0500

    scrape sold item urls from multiple pages

--- backend/backupScraper/puppetScraper.js
+++ backend/backupScraper/puppetScraper.js
@@ -3,24 +3,31 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({
       headless: false,
-      slowMo: 250,
+      slowMo: 7000,
   });
   const page = await browser.newPage();
-//   await page.goto("https://www.ebay.com/sch/i.html?_from=R40&_nkw=nike+blazer+x+sacai+x+kaws+red+men%27s+shoes&_sacat=0&LH_TitleDesc=0&rt=nc");
- 
-  await page.goto("https://www.ebay.com/sch/i.html?_from=R40&_nkw=nike+blazer+x+sacai+x+kaws+red+men%27s+shoes&_sacat=0&LH_TitleDesc=0&rt=nc&LH_Sold=1&LH_Complete=1");
-  // await page.goto("https://www.ebay.com/itm/265443428876?hash=item3dcda94a0c:g:~7EAAOSwRCthsWaD");
+  var res = [];
 
+  for (let pageNumber = 2; pageNumber < 4; pageNumber++) {
+    console.log("Page number: " + pageNumber);
+    let url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=nike+blazer+x+sacai+x+kaws+red+men%27s+shoes&_sacat=0&LH_TitleDesc=0&rt=nc&LH_Sold=1&LH_Complete=1%22&_pgn=";
+    await page.goto(url.concat(pageNumber));
+    
+    headings = await page.evaluate(() => {
+      // headings_elements = document.querySelectorAll("li.s-item.s-item__pl-on-bottom");
+      // headings_elements = document.querySelectorAll("span.POSITIVE");
+      // headings_elements = document.querySelectorAll("span.vi-inl-lnk.vi-original-listing");
+      headings_elements = document.querySelectorAll("a.s-item__link");
 
-  headings = await page.evaluate(() => {
-    // headings_elements = document.querySelectorAll("li.s-item.s-item__pl-on-bottom");
-    // headings_elements = document.querySelectorAll("span.POSITIVE");
-    headings_elements = document.querySelectorAll("a.s-item__link");
-    // headings_elements = document.querySelectorAll("span.vi-inl-lnk.vi-original-listing");
+      // total items where bid was offered: span.s-item__bids.s-item__bidCount
+      // total items where offer or buy it now was offered: span.s-item__purchase-options.s-item__purchaseOptions
 
-    headings_array = Array.from(headings_elements);
-    return headings_array.map(heading => heading.href);
-  });
-  console.log(headings);
+      headings_array = Array.from(headings_elements);
+      return headings_array.map(heading => heading.href);
+    });
+    console.log(headings);
+    res.concat(headings);
+  }
+  console.log(res);
   await browser.close();
 })();
