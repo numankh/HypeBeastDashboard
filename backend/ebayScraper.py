bd47827873790f65a6ea5d9632ca854b4481f3b2
numankh <khannuman15@gmail.com>
2022-02-28 23:05:28 -0500

    clean ebay shoe listing scraper code

--- backend/ebayScraper.py
+++ backend/ebayScraper.py
@@ -1,15 +1,13 @@
+from ntpath import join
 import requests
 from bs4 import BeautifulSoup
-import csv
 import re
-from flask import Flask, request, jsonify
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 import os
 import urllib.request
 from utils.cleaning import cleanString
 from nlp.readabilityIndex import flesch_reading_ease_score
 from nlp.readabilityIndex import average_grade_score
+from datetime import datetime
 
 rows = []
 
@@ -38,14 +36,132 @@ def shoeSizeOverride(test_string):
         if number < 20:
             return number
 
-def exportToCsv():
-    fieldnames = ['item_name', 'item_price', 'free_shipping', 'shoe_size', 'number_of_images',
-                    'seller_rating', 'adult_shoe', 'youth_shoe', 'child_shoe', 'item_url']
+def getSellerProfile(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-    with open('ebayItems.csv', 'w', encoding='UTF8', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(rows)
+    # Get username
+    sellerUsername = soup.find('a', attrs = {'class': 'mbg-id'}).text
+    sellerUsername = sellerUsername[8:]
+    # print(f"Seller username: <{sellerUsername}>")
+
+    # Get positive
+    positive = soup.find('a', attrs = {'title': 'Positive'})
+    positive = positive.find('span', attrs = {'class': 'num'}).text
+    positive = int(positive.replace(',', ''))
+    # print(f"positive: <{positive}>")
+
+    # Get neutral
+    neutral = soup.find('a', attrs = {'title': 'Neutral'})
+    neutral = neutral.find('span', attrs = {'class': 'num'}).text
+    neutral = int(neutral.replace(',', ''))
+    # print(f"neutral: <{neutral}>")
+
+    # Get negative
+    negative = soup.find('a', attrs = {'title': 'Negative'})
+    negative = negative.find('span', attrs = {'class': 'num'}).text
+    negative = int(negative.replace(',', ''))
+    # print(f"negative: <{negative}>")
+
+    # Get followers
+    followers = soup.find('span', attrs = {'class': 'hide countElem'}).get('contentstring')
+    followers = re.search('>(.*)<', followers).group(1)
+    followers = int(followers.replace(',', ''))
+    # print(f"followers: <{followers}>")
+
+    # Get positive feedback percentage
+    positive_feedback = soup.find('div', attrs = {'class': 'perctg'}).text
+    positive_feedback = positive_feedback.strip()
+    positive_feedback = positive_feedback[:positive_feedback.index("%")]
+    # print(f"positive feedback: <{positive_feedback}>")
+
+    # Get join date
+    join_date = soup.find('div', attrs = {'id': 'member_info'}).find('span', attrs = {'class': 'info'}).text
+    join_date = join_date.replace(",", "")
+    join_date = datetime.strptime(join_date, '%b %d %Y')
+
+    return {
+        "username": sellerUsername,
+        "positive": positive,
+        "neutral": neutral,
+        "negative": negative,
+        "followers": followers,
+        "positive_feedback": positive_feedback,
+        "join_date": join_date
+    }
+
+def getShoeSize(itemPageSoup, rawItemTitle):
+    shoeSize = ""
+    itemSpecificsSection = ""
+    adult_shoe = True
+    youth_shoe = False
+    child_shoe = False
+
+    # Try to get shoe size from item specifics section
+    try:
+        possibleSections = itemPageSoup.findAll('div', attrs = {'class':'ux-layout-section-module'})
+        for possibleSection in possibleSections:
+            if (possibleSection.find('h2', attrs = {'id': 'ABOUT_THIS_ITEM0-0-1-1-title'})):
+                itemSpecificsSection = possibleSection
+    except:
+        print(f"Error with obtaining shoe size on item page, check url: <{itemUrl}>")
+        pass
+    vals = [i.text for i in itemSpecificsSection.findAll('span')]
+    for t in range(0,len(vals)):
+        if "US Shoe Size" in vals[t]:
+            shoeSize = vals[t+1].upper()
+            break
+    # print(f"Scraping item section: <{shoeSize}>")
+
+    # Try to get shoe size from item title
+    if(not shoeSize):
+        temp = rawItemTitle.lower()
+        if ("sz" in temp or "size" in temp):
+            temp = temp.split(" ")
+            for i in range(len(temp)):
+                if (temp[i] == "sz" or temp[i] == "size"):
+                    shoeSize = temp[i+1]
+        if (not isFloat(shoeSize)):
+            for i in range(len(shoeSize)):
+                if (not shoeSize[i].isnumeric()):
+                    shoeSize = shoeSize[:i]
+                    break
+    # print(f"Scraping title: <{shoeSize}>")
+
+
+    # TODO: Try to get shoe size from dropdown menu
+    # if(not shoeSize):
+    #     shoeSize = itemPageSoup.find('option', attrs = {'id':'msku-opt-0'})
+    #     if(shoeSize):
+    #         shoeSize = shoeSize.text
+    #         if("[" in shoeSize):
+    #             shoeSize = shoeSize[:shoeSize.index("[")]
+    # print(f"Scraping dropdown menu: <{shoeSize}>")
+
+    # Check shoe booleans
+    if (shoeSize):
+        if 'Y' in shoeSize:
+            adult_shoe = False
+            youth_shoe = True
+            child_shoe = False
+        elif 'C' in shoeSize:
+            adult_shoe = False
+            youth_shoe = False
+            child_shoe = True
+        
+        for x in range(len(shoeSize)):
+            if shoeSize[x] != "." and not shoeSize[x].isnumeric():
+                shoeSize = shoeSize[0:x]
+                break
+
+    # print(f"FINAL SHOE SIZE: <{shoeSize}>")
+    
+    return {
+        "shoe_size": shoeSize,
+        "adult_shoe": adult_shoe,
+        "child_shoe": child_shoe,
+        "youth_shoe": youth_shoe
+    }
 
 def scrapeItemPage(item):
     # Get item title and remove all non-alphabet
@@ -81,7 +197,13 @@ def scrapeItemPage(item):
     except Exception as e:
         print(f"Error with obtaining item price, check url: <{itemUrl}>")
         pass
-    itemPrice = float(itemPrice[1:])
+
+    if("to" in itemPrice):
+        itemPrice = itemPrice[1:]
+        itemPrice = float(itemPrice[(itemPrice.index("$") + 1):])
+    else:
+        itemPrice = float(itemPrice[1:])
+
 
     # Get item shipping info
     try:
@@ -121,11 +243,17 @@ def scrapeItemPage(item):
         imgRow = imgRow.findAll('button', attrs = {'class':'pic pic1'})
         numberOfItemImgs = len(imgRow)
 
+    # Obtain seller related information
+    try:
+        sellerProfileUrl = itemPageSoup.find('div', attrs = {'class':'ux-seller-section__item--seller'}).findAll('a')[0].get('href')
+        sellerRes = getSellerProfile(sellerProfileUrl)
+    except:
+        print(f"Error with obtaining seller profile URL")
+        return {}
+
     # Obtain seller rating
     try:
         sellerRating = itemPageSoup.find('div', attrs = {'class':'ux-seller-section__item--seller'}).findAll('a')[1].text
-        # sellerRating = itemPageSoup.find('span', attrs = {'class':'mbg-l'}).find('a').text
-        # print(f"Seller rating: <{sellerRating}>")
     except Exception as e:
         print(f"Error with obtaining seller's rating on item page, check url: <{itemUrl}>")
         pass
@@ -136,50 +264,12 @@ def scrapeItemPage(item):
     iframe_soup = BeautifulSoup(response, 'html.parser')
     raw_item_description = iframe_soup.find('div', attrs = {'id':'ds_div'}).text
     item_description = cleanString(raw_item_description)
-    # print(item_description)
     desc_fre_score = flesch_reading_ease_score(item_description)
     desc_avg_grade_score = average_grade_score(item_description)
-    # print(f"FRE SCORE: <{desc_fre_score}>")
-    # print(f"AVG GRADE SCORE: <{desc_avg_grade_score}>")
 
 
     # Obtain item size
-    shoeSize = ""
-    try:
-        itemSpecificsSection = itemPageSoup.find('div', attrs = {'class':'ux-layout-section-module'}).findAll('div', attrs = {'class':'ux-layout-section__row'})
-    except Exception as e:
-        print(f"Error with obtaining shoe size on item page, check url: <{itemUrl}>")
-        pass
-    for x in itemSpecificsSection:
-        vals = [i.text for i in x.findAll('span')]
-        for t in range(0,len(vals)):
-            if "US Shoe Size" in vals[t]:
-                shoeSize = vals[t+1].upper()
-                break
-
-    # Determine if shoe size is for adults or kids and clean the string
-    oldShoeSize = shoeSize
-    adult_shoe = True
-    child_shoe = False
-    youth_shoe = False
-    for i in range(0, len(shoeSize)):
-        char = shoeSize[i]
-        if char == 'C':
-            shoeSize = shoeSize[0:i]
-            adult_shoe = False
-            child_shoe = True
-        elif char == 'Y':
-            shoeSize = shoeSize[0:i]
-            adult_shoe = False
-            youth_shoe = True
-        elif char == ',':
-            shoeSize = shoeSize[0:i]
-            break
-    
-    # Catch any issues with shoe size
-    if (not shoeSize.isdigit() and not isFloat(shoeSize)):
-        shoeSize = shoeSizeOverride(rawItemTitle)
-
+    res_shoe_size = getShoeSize(itemPageSoup, rawItemTitle)
 
     # Store fields in a dictionary
     itemData = {
@@ -193,39 +283,48 @@ def scrapeItemPage(item):
         "item_description": item_description,
         "desc_fre_score": desc_fre_score,
         "desc_avg_grade_score": desc_avg_grade_score,
-        "shoe_size": shoeSize,
-        "adult_shoe": adult_shoe,
-        "youth_shoe": youth_shoe,
-        "child_shoe": child_shoe,
         "item_url": itemUrl,
-        "model": "Sacai x KAWS x Nike Blazer Low",
+        "model": "Air Jordan 1 Retro High OG Dark Marina Blue",
         "sold_date": None,
         "sold": False
     }
+
+    itemData.update(sellerRes)
+    itemData.update(res_shoe_size)
 
     print(itemData)
     rows.append(itemData)
     print("===========================")
 
-def ebayScraperMain():
-    for ebayPageNumber in range(1,7):
-        URL = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1312&_nkw=kaws+sacai+nike+blazer+low+red&_sacat=0&_pgn={ebayPageNumber}"
-        # URL = f"https://www.ebay.com/sch/i.html?_from=R40&_nkw=nike+dunk+mid+social+status+chocolate+milk&_sacat=&rt=nc&LH_ItemCondition=1000&_pgn={ebayPageNumber}" 
+def ebayScraper(shoe_name):
+    for pageNumber in range(1,2):
+        # Obtain ebay search url for the shoe name provided
+        processed_shoe_name = shoe_name.replace(" ","+")
+        URL = f"https://www.ebay.com/sch/i.html?_nkw={processed_shoe_name}&_pgn={pageNumber}"
+
+        # Call the URL
         r = requests.get(URL)
         soup = BeautifulSoup(r.content, 'html.parser')
-
         print(f"Currently scraping this url: <{URL}>")
         
-        pageData = soup.findAll('li', attrs = {'class':'s-item s-item__pl-on-bottom s-item--watch-at-corner'})
-        
-        if (pageData):
-            for item in pageData:
-                scrapeItemPage(item)
+        items = soup.findAll('div', attrs = {'class':'s-item__info clearfix'})
+        # items = soup.findAll('li', attrs = {'class':'s-item s-item__pl-on-bottom s-item--watch-at-corner'})
+        if (items):
+            for item in items:
+                itemUrl = item.find('a', attrs = {'class':'s-item__link'}).get('href')
+                itemPageSoup = BeautifulSoup(requests.get(itemUrl).content, 'html.parser')
+                noItemFoundPage = itemPageSoup.find('p', attrs = {'class': 'error-header__headline'})
+
+                if(noItemFoundPage):
+                    print("NO ITEM FOUND PAGE")
+                    continue
+                else:
+                    scrapeItemPage(item)
         else:
             print("ERROR: unable to scrape item list page")
-        print("=============================================")
 
+        break
     return rows
 
 if __name__ == "__main__":
-    ebayScraperMain()
+    ebayScraper("Air Jordan 1 Retro High OG Dark Marina Blue")
